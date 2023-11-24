8ee2bd0698ebfcca21f2a267b2b10187e311f454
mykolamateichuk <mateichukmykola@gmail.com>
2023-11-24 16:21:17 +0200

    Remove secret key for deployment

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -7,7 +7,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-9jim!(&m3s(4a)y&ca)x-^rxqz578ixt61-)nx&gt0@t7jy4)r")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
